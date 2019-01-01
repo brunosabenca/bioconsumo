dd06d99e6d5ec2f784a5792498171205d609b0cd
Bruno Sabença <bruno@brunosabenca.com>
2019-01-01 19:20:31 +0000

    Removes delete button from product listing

--- resources/views/products/_list.blade.php
+++ resources/views/products/_list.blade.php
@@ -3,12 +3,6 @@
     <div class="card-body">
         <h5 class="card-title"><a href="/products/{{ $product->id }}">{{ $product->name }}</a></h5>
         <p class="card-text">{{ $product->description }}</p>
-            <form action="{{ $product->path() }}" method="POST">
-                {{ csrf_field() }}
-                {{ method_field('DELETE') }}
-
-                <button type="submit" class="btn btn-danger">Delete</button>
-            </form>
     </div>
 </div>
 <br/>
