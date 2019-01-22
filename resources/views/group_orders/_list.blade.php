900b8de6efbd63aa740cfe4bebecf81aa5253051
Bruno Sabença <bruno@brunosabenca.com>
2019-01-22 17:49:34 +0000

    Removes leftover code

--- resources/views/group_orders/_list.blade.php
+++ resources/views/group_orders/_list.blade.php
@@ -2,7 +2,7 @@
 <div class="card">
     <div class="card-body">
         <div class="level">
-            <h5 class="card-title">{{ $order->status() }}<a href="{{ $order->path() }}">Order #{{ $order->id }}: {{ $order->open_date }} to {{ $order->close_date }}</a> <span class="text-danger text-uppercase small">{{ $order->cancelled == true ? 'Cancelled' : ''}}</span></h5>
+            <h5 class="card-title"><a href="{{ $order->path() }}">Order #{{ $order->id }}: {{ $order->open_date }} to {{ $order->close_date }}</a> <span class="text-danger text-uppercase small">{{ $order->cancelled == true ? 'Cancelled' : ''}}</span></h5>
         </div>
     </div>
 </div>
