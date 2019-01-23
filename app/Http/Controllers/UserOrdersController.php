cb48111e74bb213bd04bdc0f95ad5840d5f540ff
Bruno Sabença <bruno@brunosabenca.com>
2019-01-23 11:09:14 +0000

    Refactors #12932f

--- app/Http/Controllers/UserOrdersController.php
+++ app/Http/Controllers/UserOrdersController.php
@@ -25,26 +25,34 @@ class UserOrdersController extends Controller
      */
     public function create()
     {
+        $user_order = auth()->user()->orders()->where('delivered', false)->first();
         $group_order = GroupOrder::where('cancelled', false)->latest()->first();
 
-        $user_order = auth()->user()->orders()->where('delivered', false)->first();
-
         if ($user_order === null) {
+            $group_order = GroupOrder::where('cancelled', false)->latest()->first();
             $user_order = UserOrder::create([
                 'group_order_id' => $group_order->id,
                 'user_id' => auth()->user()->id
             ]);
-        } else if ($group_order !== null) {
-            $current_group_order = GroupOrder::where('id', $user_order->group_order_id)->first();
-
-            if ($current_group_order->cancelled) {
-                $user_order->group_order_id = $group_order->id;
-            }
+        } elseif ($group_order !== null) {
+            $this->update_group_order($user_order, $group_order);
         }
+
 
         return view('user_orders.show', [
             'group_order' => $group_order
         ]);
+    }
+
+    public function update_group_order(UserOrder $user_order, GroupOrder $group_order)
+    {
+        if ($group_order !== null) {
+            $current_group_order = GroupOrder::where('id', $user_order->group_order_id)->first();
+
+            if ($group_order->cancelled) {
+                $user_order->group_order_id = $group_order->id;
+            }
+        }
     }
 
     /**
