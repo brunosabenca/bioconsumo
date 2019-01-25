e82fbeb06f7f54b8457b70f3a15e25f3d9c37b0c
Bruno Sabença <bruno@brunosabenca.com>
2019-01-25 10:12:35 +0000

    Adds route to allow logging user out with get request

--- routes/web.php
+++ routes/web.php
@@ -38,4 +38,6 @@ Route::get('/cart', 'CartController@index');
 
 Auth::routes();
 
+Route::get('/logout', 'Auth\LoginController@logout')->name('logout');
+
 Route::get('/home', 'HomeController@index')->name('home');
