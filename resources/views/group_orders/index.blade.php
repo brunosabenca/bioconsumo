545f73f456b183b342f04973b8de993597d448d7
Bruno Sabença <bruno@brunosabenca.com>
2019-01-31 17:53:41 +0000

    Adds create new group order button, fixes #65

--- resources/views/group_orders/index.blade.php
+++ resources/views/group_orders/index.blade.php
@@ -2,7 +2,9 @@
 
 @section('content')
 <div class="container-fluid">
-    <h2>Group Orders</h2>
+    <h2>Group Orders
+        <a href="/orders/create" role="button" class="small" aria-label="Create new group order"><i class="fa fa-plus-circle" aria-hidden="true"></i></a>
+    </h2>
     <div class="row">
         <div class="col-md-12">
             @include('group_orders._list')
