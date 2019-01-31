cd805ecd44c1b719ee8c488d0e79c0d8db020b42
Bruno Sabença <bruno@brunosabenca.com>
2019-01-31 16:14:37 +0000

    Adds success feedback to new order page, fixes #72

--- resources/js/pages/NewGroupOrder.vue
+++ resources/js/pages/NewGroupOrder.vue
@@ -22,7 +22,7 @@
 
         filters: {
             date: function (date) {
-                return moment(date).format('YYYY MM DD');
+                return moment(date).format('YYYY-MM-DD');
             }
         },
 
@@ -36,18 +36,28 @@
             },
 
             postAddNewGroupOrder() {
+                var searchUrl = this.uri
                 return axios.post(this.uri, this.form).then( (response) =>  {
                     this.resetForm();
                     this.submitted = true;
                 }).catch( (error) => {
-                    this.errors = error.response.data.errors;
+                    // Error
+                    if (error.response) {
+                        this.submitted = false;
+                        this.errors = error.response.data.errors;
+                    } else if (error.request) {
+                        console.log(error.request);
+                    } else {
+                        console.log('Error', error.message);
+                    }
                 });
             },
 
             resetForm() {
+                this.submitted = false;
+                this.errors = [];
                 this.form.open_date = moment().format('YYYY-MM-DD');
                 this.form.close_date = moment().add(3, 'days').format('YYYY-MM-DD');
-                this.errors = [];
                 this.form.active_sellers = [];
             },
 
--- resources/views/group_orders/create.blade.php
+++ resources/views/group_orders/create.blade.php
@@ -6,44 +6,47 @@
     <h2>Create new group order</h4>
     <div class="row">
         <div class="col-sm-6 col-md-4 col-xl-3 mb-3">
-                <div class="form-group">
-                    <label for="open-date">Open Date</label>
-                    <input class="date form-control" :class="errors.open_date ? 'is-invalid' : ''" id="open-date" type="date" name="open-date" v-model="form.open_date"> 
-                    <form-error v-if="errors.open_date" :errors="errors">
-                        @{{ errors.open_date[0] }}
-                    </form-error>
+            <div class="alert alert-success" v-if="submitted">
+                Group order successfully created!
+            </div>
+            <div class="form-group">
+                <label for="open-date">Open Date</label>
+                <input class="date form-control" :class="errors.open_date ? 'is-invalid' : ''" id="open-date" type="date" name="open-date" v-model="form.open_date"> 
+                <form-error v-if="errors.open_date" :errors="errors">
+                    @{{ errors.open_date[0] }}
+                </form-error>
+            </div>
+
+            <div class="form-group">
+                <label for="close-date">Close Date</label>
+                <input class="date form-control" :class="errors.close_date ? 'is-invalid' : ''" id="close-date" type="date" name="close-date" v-model="form.close_date">
+                <form-error v-if="errors.close_date" :errors="errors">
+                    @{{ errors.close_date[0] }}
+                </form-error>
+            </div>
+
+            <div class="form-group">
+                <label for="active-sellers">Active Sellers</label>
+
+                <div class="form-check" :class="errors.active_sellers ? 'is-invalid' : ''" id="active-sellers" name="active-sellers" v-for="seller in sellers">
+                    <input class="form-check-input" :class="errors.active_sellers ? 'is-invalid' : ''" type="checkbox" :value="seller.id" :name="`seller-${seller.id}`"  v-model="form.active_sellers">
+                    <label class="form-check-label" :for="`seller-${seller.id}`">
+                        @{{seller.name}}
+                    </label>
+
+                    <template v-if="errors.active_sellers">
+                        <form-error :errors="errors">
+                            @{{ errors.active_sellers[0] }}
+                        </form-error>
+                    </template>
+                    <template v-else>
+                    </template>
                 </div>
+            </div>
 
-                <div class="form-group">
-                    <label for="close-date">Close Date</label>
-                    <input class="date form-control" :class="errors.close_date ? 'is-invalid' : ''" id="close-date" type="date" name="close-date" v-model="form.close_date">
-                    <form-error v-if="errors.close_date" :errors="errors">
-                        @{{ errors.close_date[0] }}
-                    </form-error>
-                </div>
-
-                <div class="form-group">
-                    <label for="active-sellers">Active Sellers</label>
-
-                    <div class="form-check" :class="errors.active_sellers ? 'is-invalid' : ''" id="active-sellers" name="active-sellers" v-for="seller in sellers">
-                        <input class="form-check-input" :class="errors.active_sellers ? 'is-invalid' : ''" type="checkbox" :value="seller.id" :name="`seller-${seller.id}`"  v-model="form.active_sellers">
-                        <label class="form-check-label" :for="`seller-${seller.id}`">
-                            @{{seller.name}}
-                        </label>
-
-                        <template v-if="errors.active_sellers">
-                            <form-error :errors="errors">
-                                @{{ errors.active_sellers[0] }}
-                            </form-error>
-                        </template>
-                        <template v-else>
-                        </template>
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <button type="submit" class="btn btn-primary" @click="postAddNewGroupOrder">Submit</button>
-                </div>
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary" @click="postAddNewGroupOrder">Submit</button>
+            </div>
         </div>
     </div>
 </div>
