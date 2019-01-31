d2f3d1567bb12240b2eaac239a8a3a9b93bd4a71
Bruno Sabença <bruno@brunosabenca.com>
2019-01-31 17:54:14 +0000

    Adds create new product button, fixes #64

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -4,7 +4,9 @@
 <product-listing-view :products="{{ $products }}" inline-template>
 
 <div class="container-fluid">
-    <h2>Products</h2>
+    <h2>Products
+        <a href="/products/create" role="button" class="small" aria-label="Create new product"><i class="fa fa-plus-circle" aria-hidden="true"></i></a>
+    </h2>
 
     <div class="row" v-for="chunk in chunks">
         <div class="col-md-4" v-for="product in chunk" :key="product.id">
