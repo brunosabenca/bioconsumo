361e9bd25d7ad7d4b5403d47cdc6dd5a4e40d512
Bruno Sabença <bruno@brunosabenca.com>
2019-02-08 12:33:33 +0000

    Changes indentation

--- resources/js/components/ProductListingItem.vue
+++ resources/js/components/ProductListingItem.vue
@@ -1,105 +1,104 @@
 <template>
 <!-- Editing -->
 <div>
-        <div class="card" v-show="editing">
-            <img class="card-img-top img-responsive" :src="imagePath">
-            <div class="card-body">
-                <div class="form-group">
-                    <div class="form-row">
-                        <label for="name">Name</label>
-                        <input type="text" class="card-title form-control" id="name" v-model="form.name" maxlength="80"/>
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <div class="form-row">
-                        <label for="description">Description</label>
-                        <textarea class="card-text form-control" id="description" v-model="form.description" maxlength="255" rows="4"></textarea>
-                    </div>
-                </div>
-
+    <div class="card" v-show="editing">
+        <img class="card-img-top img-responsive" :src="imagePath">
+        <div class="card-body">
+            <div class="form-group">
                 <div class="form-row">
-                    <div class="form-group">
-                        <div class="form-col">
-                            <label for="stock">Stock</label>
-                            <input type="text" class="card-title form-control" v-model="form.stock"  maxlength="4"/>
-                        </div>
-                    </div>
-                    <div class="form-group ml-a">
-                        <div class="form-col">
-                            <label for="price">Price / <span v-text="stock_unit_type"></span></label>
-                            <money class="form-control" v-model="form.price" v-bind="money"></money>
-                        </div>
-                    </div>
+                    <label for="name">Name</label>
+                    <input type="text" class="card-title form-control" id="name" v-model="form.name" maxlength="80"/>
                 </div>
             </div>
 
-            <div class="card-footer d-flex flex-row align-items-center">
-                <button class="btn btn-danger btn-sm" v-show="authorize('owns', product) || authorize('can', 'delete any product')"
-                    @click="destroy" aria-label="Delete" data-toggle="tooltip" title="Delete this product">
-                    <i class="fa fa-trash" aria-hidden="true"></i>
-                </button>
-                <button class="btn btn-secondary btn-sm ml-a mr-1" @click="resetForm"
-                    data-toggle="tooltip" title="Cancel changes">
-                    <i class="fa fa-close" aria-hidden="true"></i> Cancel
-                </button>
-                <button class="btn btn-primary btn-sm" @click="update"
-                    data-toggle="tooltip" title="Save changes">
-                    <i class="fa fa-save" aria-hidden="true"></i>
-                    Save
-                </button>
-            </div>
-        </div>
-        <!-- Viewing -->
-        <div class="card" v-show="! editing">
-            <img class="card-img-top img-responsive" :src="imagePath">
-            <h5 class="card-header level">
-                <span class="text-muted" v-if="seller"><span v-text="seller"></span>'s</span>
-                <span class="ml-1" v-text="name" v-if="single"></span>
-                <a class="ml-1" v-text="name" :href="`/products/${product.id}`" v-else></a>
-                <span class="ml-a">
-                    <span v-text="price"></span>
-                    <span class="ml-1 small"> / <span v-text="stock_unit_type"></span></span>
-                </span>
-            </h5>
-
-            <div class="card-body">
-                <p class="card-text"><span v-text="description"></span></p>
-                <p class="card-text">Stock: <span v-text="stock"></span></p>
-            </div>
-
-            <div class="card-footer d-flex flex-row align-items-center">
-                <div class="flex-grow-1 ml-1">
-                    <button class="flex-grow-1 btn btn-secondary btn-sm" v-show="authorize('owns', product) || authorize('can','edit any product')"
-                        @click="editing = true" data-toggle="tooltip" title="Edit this product">
-                            <i class="fa fa-edit" aria-hidden="true"></i> Edit
-                    </button>
+            <div class="form-group">
+                <div class="form-row">
+                    <label for="description">Description</label>
+                    <textarea class="card-text form-control" id="description" v-model="form.description" maxlength="255" rows="4"></textarea>
                 </div>
+            </div>
 
-                <div class="flex-grow-0 ml-a">
-                    <div class="input-group add-to-cart" v-show="stock > 0 && authorize('can','add items to cart')">
-                        <vue-numeric
-                            class="form-control"
-                            value="0"
-                            :currency="unit_symbol"
-                            currency-symbol-position="suffix"
-                            :minus="false"
-                            :precision="unit_precision"
-                            :empty-value="0"
-                            v-bind:min="0"
-                            v-bind:max="10000"
-                            v-model.number="form.quantity"
-                            data-toggle="tooltip"
-                            title="Quantity"></vue-numeric>
-                        <div class="input-group-append">
-                            <button class="btn btn-primary" @click="addToCart" aria-label="Add product to order" data-toggle="tooltip" title="Add product to your order">
-                                <i class="fa fa-cart-plus" aria-hidden="true"></i>
-                            </button>
-                        </div>
+            <div class="form-row">
+                <div class="form-group">
+                    <div class="form-col">
+                        <label for="stock">Stock</label>
+                        <input type="text" class="card-title form-control" v-model="form.stock"  maxlength="4"/>
+                    </div>
+                </div>
+                <div class="form-group ml-a">
+                    <div class="form-col">
+                        <label for="price">Price / <span v-text="stock_unit_type"></span></label>
+                        <money class="form-control" v-model="form.price" v-bind="money"></money>
                     </div>
                 </div>
             </div>
         </div>
+
+        <div class="card-footer d-flex flex-row align-items-center">
+            <button class="btn btn-danger btn-sm" v-show="authorize('owns', product) || authorize('can', 'delete any product')"
+                @click="destroy" aria-label="Delete" data-toggle="tooltip" title="Delete this product">
+                <i class="fa fa-trash" aria-hidden="true"></i>
+            </button>
+            <button class="btn btn-secondary btn-sm ml-a mr-1" @click="resetForm"
+                data-toggle="tooltip" title="Cancel changes">
+                <i class="fa fa-close" aria-hidden="true"></i> Cancel
+            </button>
+            <button class="btn btn-primary btn-sm" @click="update"
+                data-toggle="tooltip" title="Save changes">
+                <i class="fa fa-save" aria-hidden="true"></i> Save
+            </button>
+        </div>
+    </div>
+    <!-- Viewing -->
+    <div class="card" v-show="! editing">
+        <img class="card-img-top img-responsive" :src="imagePath">
+        <h5 class="card-header level">
+            <span class="text-muted" v-if="seller"><span v-text="seller"></span>'s</span>
+            <span class="ml-1" v-text="name" v-if="single"></span>
+            <a class="ml-1" v-text="name" :href="`/products/${product.id}`" v-else></a>
+            <span class="ml-a">
+                <span v-text="price"></span>
+                <span class="ml-1 small"> / <span v-text="stock_unit_type"></span></span>
+            </span>
+        </h5>
+
+        <div class="card-body">
+            <p class="card-text"><span v-text="description"></span></p>
+            <p class="card-text">Stock: <span v-text="stock"></span></p>
+        </div>
+
+        <div class="card-footer d-flex flex-row align-items-center">
+            <div class="flex-grow-1 ml-1">
+                <button class="flex-grow-1 btn btn-secondary btn-sm" v-show="authorize('owns', product) || authorize('can','edit any product')"
+                    @click="editing = true" data-toggle="tooltip" title="Edit this product">
+                        <i class="fa fa-edit" aria-hidden="true"></i> Edit
+                </button>
+            </div>
+
+            <div class="flex-grow-0 ml-a">
+                <div class="input-group add-to-cart" v-show="stock > 0 && authorize('can','add items to cart')">
+                    <vue-numeric
+                        class="form-control"
+                        value="0"
+                        :currency="unit_symbol"
+                        currency-symbol-position="suffix"
+                        :minus="false"
+                        :precision="unit_precision"
+                        :empty-value="0"
+                        v-bind:min="0"
+                        v-bind:max="10000"
+                        v-model.number="form.quantity"
+                        data-toggle="tooltip"
+                        title="Quantity"></vue-numeric>
+                    <div class="input-group-append">
+                        <button class="btn btn-primary" @click="addToCart" aria-label="Add product to order" data-toggle="tooltip" title="Add product to your order">
+                            <i class="fa fa-cart-plus" aria-hidden="true"></i>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 </template>
 
